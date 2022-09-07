428b06a84f4a05765acad29982563e70071722e5
veerabhadra swamy uppu <78792323+ultra-hash@users.noreply.github.com>
2022-09-07 10:24:01 +0530

    Update createSql.py

--- templates/createSql.py
+++ templates/createSql.py
@@ -7,7 +7,7 @@ username = sys.argv[1]
 password = sys.argv[2]
 database = username + '_db'
 create = f"""CREATE DATABASE IF NOT EXISTS {database} DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-CREATE USER IF NOT EXISTS {username} IDENTIFIED BY {password};
+CREATE USER IF NOT EXISTS {username}@'%' IDENTIFIED BY '{password}';
 GRANT CREATE, ALTER, DROP, INSERT, UPDATE, DELETE, SELECT ON {database}.* TO {username}@'%';
 FLUSH PRIVILEGES;
 exit"""
